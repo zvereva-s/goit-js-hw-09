09ef423aa4cfc346f2ac8fd58b7a2443ac3a96b9
zvereva <96833638+zvereva-s@users.noreply.github.com>
2022-05-30 15:44:45 +0300

    completed task 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
 
+//======== refs ========//
+const refs = {
+    body: document.querySelector('body'),
+    btnStart: document.querySelector('[data-start]'),
+    btnStop: document.querySelector('[data-stop]'),
+}
+
+let timerId = null;
+
+refs.btnStart.addEventListener('click', () => {
+    timerId = setInterval(() => {
+    return changeBcgColor()}, 1000)
+});
+
+refs.btnStop.addEventListener('click', () => {
+    clearInterval(timerId)
+});
+
+function changeBcgColor() {
+    refs.body.style.backgroundColor = getRandomHexColor();
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+
