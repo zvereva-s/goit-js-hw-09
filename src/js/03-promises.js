d285ce479b329e16bb514b545f9696776a999a6b
zvereva <96833638+zvereva-s@users.noreply.github.com>
2022-05-31 23:06:40 +0300

    completed task 03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+//! position - номер создаваемого промиса;
+
+const form = document.querySelector('.form');
+const { delay, amount, step } = form.elements;
+
+
+form.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(e) {
+  e.preventDefault();
+  let counter = 0;
+  let pos = 1;
+  while (counter < amount.valueAsNumber) {
+
+    createPromise(pos, delay.valueAsNumber).then(({position, timeDelay}) => {
+    Notify.success(`✅ Fulfilled promise ${position} in ${timeDelay}ms`);
+  })
+    .catch(({position, timeDelay}) => {
+      Notify.failure(`❌ Rejected promise ${position} in ${timeDelay}ms`);
+    })
+
+    counter += 1;
+    pos += 1;
+    delay.valueAsNumber += step.valueAsNumber;
   }
+}
+
+function createPromise(position, timeDelay) { 
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3; 
+      if (shouldResolve) {
+        resolve({position, timeDelay});
+      } else {
+        reject({position, timeDelay});
+      }
+    }, timeDelay);
+  });
+  return promise;
 }
